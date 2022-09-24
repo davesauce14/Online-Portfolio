3f401f2fa4b0e488966787096cc34931f0c17a94
Dave Gould <davegould@microsoft.com>
2022-09-24 11:54:52 -0700

    Change Urls To Azure

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,6 +1,6 @@
 <template>
 
     <v-container class="fill-height" fluid >
-        <iframe class="big-iframe" src="http://dave.digital-resume-generator.net/" frameborder="0"></iframe>
+        <iframe class="big-iframe" src="https://zealous-coast-0ef770910.1.azurestaticapps.net/" frameborder="0"></iframe>
       </v-container>
 </template>
